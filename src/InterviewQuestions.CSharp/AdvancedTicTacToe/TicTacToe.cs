6230fd0f898a43d2cfbb5ecb062b4a226981aeef
RJ Hollberg <rj.hollberg@outlook.com>
2021-04-26 09:45:23 -0600

    Add parameter validation to 'PlacePiece' method.

--- src/InterviewQuestions.CSharp/AdvancedTicTacToe/TicTacToe.cs
+++ src/InterviewQuestions.CSharp/AdvancedTicTacToe/TicTacToe.cs
@@ -5,8 +5,14 @@ namespace InterviewQuestions.CSharp.AdvancedTicTacToe
     public class TicTacToe
     {
         public const int MinimumGameBoardSize = 2;
+        public const int Player1 = 1;
+        public const int Player2 = 2;
 
         private readonly int[,] _board;
+        private readonly int _maxIndex;
+        private readonly int _size;
+
+        private int _lastPlayer;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="TicTacToe"/> game board.
@@ -20,17 +26,65 @@ namespace InterviewQuestions.CSharp.AdvancedTicTacToe
                 throw new ArgumentOutOfRangeException(nameof(n));
 
             _board = new int[n, n];
+            _maxIndex = n - 1;  // Adjust for zero-based indexes
+            _size = n;
         }
 
         /// <summary>
-        /// Place a piece on the game board
+        /// Places a piece on the game board.
         /// </summary>
-        /// <param name="row">row to place a piece</param>
-        /// <param name="col">column to place a piece</param>
-        /// <param name="player">the player (1 or 2) the piece is for</param>
-        /// <returns>0 = no winner, 1 = player 1 won, 2 = player 2 won</returns>
+        /// <param name="row">The row in which to place a piece.</param>
+        /// <param name="col">The column in which to place a piece.</param>
+        /// <param name="player">The player (1 or 2) placing the piece.</param>
+        /// <returns>A value indicating the result of the placement:
+        /// <list type="table">
+        ///   <listheader>
+        ///     <term>Value</term>
+        ///     <description>Description</description>
+        ///   </listheader>
+        ///   <item>
+        ///     <term>0</term>
+        ///     <description>No winner yet</description>
+        ///   </item>
+        ///   <item>
+        ///     <term>1</term>
+        ///     <description>Player 1 won</description>
+        ///   </item>
+        ///   <item>
+        ///     <term>2</term>
+        ///     <description>Player 2 won</description>
+        ///   </item>
+        /// </list></returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="row"/> is not valid.
+        /// -or- <paramref name="col"/> is not valid.
+        /// -or- <paramref name="player"/> is not valid.</exception>
+        /// <exception cref="InvalidOperationException">
+        /// <paramref name="player"/> is attempting to move twice in a row.
+        /// -or- The specified coordinates are already taken.</exception>
         public int PlacePiece(int row, int col, int player)
         {
+            // Adjust coordinates for zero-based indexes
+            if (--row < 0)
+                throw new ArgumentOutOfRangeException(nameof(row), $"Row {row} is less than 1.");
+            if (--col < 0)
+                throw new ArgumentOutOfRangeException(nameof(col), $"Col {col} is less than 1.");
+            if (row > _maxIndex)
+                throw new ArgumentOutOfRangeException(nameof(row), $"Row {row} is greater than {_size}.");
+            if (col > _maxIndex)
+                throw new ArgumentOutOfRangeException(nameof(col), $"Col {col} is greater than {_size}.");
+
+            if (player != Player1 && player != Player2)
+                throw new ArgumentOutOfRangeException(nameof(player), $"Player {player} is not recognized.");
+            if (player == _lastPlayer)
+                throw new InvalidOperationException($"Player {player} is attempting to move twice in a row.");
+            _lastPlayer = player;
+
+            if (_board[row, col] != 0)
+                throw new InvalidOperationException($"A piece has already been placed at [{row},{col}].");
+            _board[row, col] = player;
+
+            // No win yet
             return 0;
         }
     }
--- test/InterviewQuestions.Tests/CSharp/AdvancedTicTacToe/TicTacToeTests.cs
+++ test/InterviewQuestions.Tests/CSharp/AdvancedTicTacToe/TicTacToeTests.cs
@@ -19,5 +19,92 @@ namespace InterviewQuestions.CSharp.AdvancedTicTacToe
             // Assert
             action.Should().Throw<ArgumentOutOfRangeException>();
         }
+
+        [Theory]
+        [InlineData(2, 0)]
+        [InlineData(2, 3)]
+        [InlineData(3, 0)]
+        [InlineData(3, 4)]
+        [InlineData(4, 0)]
+        [InlineData(4, 5)]
+        public void PlacePiece_ShouldValidateRow(int boardSize, int row)
+        {
+            // Arrange
+            var subject = new TicTacToe(boardSize);
+
+            // Act
+            Action action = () => _ = subject.PlacePiece(row, 1, TicTacToe.Player1);
+
+            // Assert
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Theory]
+        [InlineData(2, 0)]
+        [InlineData(2, 3)]
+        [InlineData(3, 0)]
+        [InlineData(3, 4)]
+        [InlineData(4, 0)]
+        [InlineData(4, 5)]
+        public void PlacePiece_ShouldValidateColumn(int boardSize, int col)
+        {
+            // Arrange
+            var subject = new TicTacToe(boardSize);
+
+            // Act
+            Action action = () => _ = subject.PlacePiece(1, col, TicTacToe.Player1);
+
+            // Assert
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Theory]
+        [InlineData(TicTacToe.Player1, TicTacToe.Player2)]
+        [InlineData(TicTacToe.Player2, TicTacToe.Player1)]
+        public void PlacePiece_ShouldValidatePlayer(
+            int startingPlayer,
+            int otherPlayer)
+        {
+            // Arrange
+            var subject = new TicTacToe(3);
+
+            // These will help the tests be more concise
+            static void OutOfRange(Action move) =>
+                move.Should().Throw<ArgumentOutOfRangeException>();
+            static void Invalid(Action move) =>
+                move.Should().Throw<InvalidOperationException>();
+
+            // Act 1 ('starting' player must be valid)
+            OutOfRange(() => _ = subject.PlacePiece(1, 1, 0));
+            OutOfRange(() => _ = subject.PlacePiece(1, 1, 3));
+            subject.PlacePiece(1, 1, startingPlayer).Should().Be(0);
+
+            // Act 2 (next player must be 'other' player)
+            OutOfRange(() => _ = subject.PlacePiece(2, 2, 0));
+            OutOfRange(() => _ = subject.PlacePiece(2, 2, 3));
+            Invalid(() => _ = subject.PlacePiece(2, 2, startingPlayer));
+            subject.PlacePiece(2, 2, otherPlayer).Should().Be(0);
+
+            // Act 3 (next player must be 'starting' player again)
+            OutOfRange(() => _ = subject.PlacePiece(3, 3, 0));
+            OutOfRange(() => _ = subject.PlacePiece(3, 3, 3));
+            Invalid(() => _ = subject.PlacePiece(3, 3, otherPlayer));
+            subject.PlacePiece(3, 3, startingPlayer).Should().Be(0);
+        }
+
+        [Fact]
+        public void PlacePiece_ShouldNotAllowPieceToBeOverwritten()
+        {
+            // Arrange
+            var subject = new TicTacToe(3);
+
+            subject.PlacePiece(1, 1, TicTacToe.Player1).Should().Be(0);
+
+            // Act
+            Action action = () => _ = subject.PlacePiece(1, 1, TicTacToe.Player2);
+
+            // Assert
+            action.Should().Throw<InvalidOperationException>();
+        }
     }
 }
